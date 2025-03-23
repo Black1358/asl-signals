faa0ad54e5e835d3c1e7ec50e0b7a4bc89d51b84
Black1358 <122863429+Black1358@users.noreply.github.com>
2025-03-24 00:31:47 +0300

    Revert "simplify the creation of effects"
    This reverts commit f9b56734aa1f435b31e73e74e43b5e701060b7d2.

--- src/errors.ts
+++ src/errors.ts
@@ -1,5 +1,5 @@
 /**
- * `$effect()` cannot be used inside an effect cleanup function
+ * `%rune%` cannot be used inside an effect cleanup function
  */
 export function effect_in_teardown(): never {
 	throw new Error(`https://svelte.dev/e/effect_in_teardown`);
@@ -13,7 +13,7 @@ export function effect_in_unowned_derived(): never {
 }
 
 /**
- * `$effect()` can only be used inside an `$effect.root()`
+ * `%rune%` can only be used inside an effect (e.g. during component initialisation)
  */
 export function effect_orphan(): never {
 	throw new Error(`https://svelte.dev/e/effect_orphan`);
